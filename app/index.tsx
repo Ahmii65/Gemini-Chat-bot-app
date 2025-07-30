b2b42342402aaad910cc6e27b07d01ec366cee36
Ahmed Ch <ch469390@gmail.com>
2025-07-30 21:37:11 +0500

    chat screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,40 @@
-import { Text, View } from "react-native";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import Textinput from "../components/textinput";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen howdy.</Text>
+    <View style={{ flex: 1, paddingVertical: 40 }}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "android" ? "height" : "padding"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 0}
+        style={{
+          flex: 1,
+        }}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={{ flex: 1 }}>
+            <Text
+              style={{
+                fontSize: 15,
+                padding: 10,
+                fontWeight: "600",
+                textAlign: "center",
+              }}
+            >
+              Chat With Gemini
+            </Text>
+            <View style={{}}></View>
+          </View>
+        </TouchableWithoutFeedback>
+        <Textinput />
+      </KeyboardAvoidingView>
     </View>
   );
 }
